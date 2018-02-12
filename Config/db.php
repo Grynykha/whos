d3645f27d8f69a76b849470d48e7cc42a12e09cd
Roman Grynykha <gromaster@gmail.com>
2018-02-12 04:13:22 +0200

    Second

---
+++ Config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+// Основна БД
+define('MYSQL_SERVER', '');
+define('MYSQL_USER', '');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', '');
+
+//// БД для логів
+define('MYSQL_SERVER_LOG', '');
+define('MYSQL_USER_LOG', '');
+define('MYSQL_PASSWORD_LOG', '');
+define('MYSQL_DB_LOG', '');
+
+// БД для адміністрування роботи парсера
+define('MYSQL_SERVER_ADM', '');
+define('MYSQL_USER_ADM', '');
+define('MYSQL_PASSWORD_ADM', '');
+define('MYSQL_DB_ADM', '');
